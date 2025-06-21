00225f589284dbfddb67b97c4dcb10494de9ce4e
Sven Goerdes <Sven@Goerdes.com>
2025-06-21 14:42:36 +0100

    added a node that converts na values to 'unknown

--- src/wine_project/pipelines/data_preprocessing/nodes.py
+++ src/wine_project/pipelines/data_preprocessing/nodes.py
@@ -7,7 +7,19 @@ import pandas as pd
 from great_expectations.core import ExpectationSuite, ExpectationConfiguration
 import typing as t
 
-def drop_col(parameters: t.Dict, df: pd.DataFrame) -> pd.DataFrame:
-    return df.drop(columns = parameters)
+# def drop_col(parameters: t.Dict, df: pd.DataFrame) -> pd.DataFrame:
+    # return df.drop(columns = parameters)
 
+
+def na_col_to_unknown(df: pd.DataFrame, parameters: t.Dict[str, t.Any]) -> pd.DataFrame:
+   
+    for col in parameters:
+        # It's good practice to check if the column exists in the DataFrame
+        if col in df.columns:
+            df[col] = df[col].fillna('unknown')
+        else:
+            # You could add a log or a print statement here to warn about missing columns
+            print(f"Warning: Column '{col}' not found in the DataFrame and was skipped.")
+
+    return df
     
