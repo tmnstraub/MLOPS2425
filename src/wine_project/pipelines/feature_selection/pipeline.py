d281ff336cad1aac36315a60ae777c74830a15dd
scarlson-sketch <s.a.carlson3rd@gmail.com>
2025-06-27 09:48:47 +0100

    update feature_selection

--- src/wine_project/pipelines/feature_selection/pipeline.py
+++ src/wine_project/pipelines/feature_selection/pipeline.py
@@ -1,47 +1,22 @@
-# src/wine_project/pipelines/feature_selection/pipeline.py
+
+"""
+This is a boilerplate pipeline
+generated using Kedro 0.18.8
+"""
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import (
-    feature_selection_rfe,
-    correlation_feature_selection,
-    intersect_features,
-)
+
+from .nodes import feature_selection
+
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
-                func=feature_selection_rfe,
-                inputs=[
-                    "X_train_data",
-                    "y_train_data",
-                    "champion_model", # <--- Input from catalog.yml
-                    "params:feature_selection_params"
-                ],
-                outputs="rfe_selected_features",
-                name="rfe_feature_selection_node",
-            ),
-            node(
-                func=correlation_feature_selection,
-                inputs=[
-                    "X_train_data",
-                    "y_train_data",
-                    "params:feature_selection_params"
-                ],
-                outputs="corr_selected_features",
-                name="correlation_feature_selection_node",
-            ),
-            node(
-                func=intersect_features,
-                inputs=["rfe_selected_features", "corr_selected_features"],
-                outputs="best_columns", # This is the final output
-                name="intersect_feature_lists",
-            ),
-            node(
-                func=lambda df, cols: df[cols],
-                inputs=["X_train_data", "best_columns"],
-                outputs="X_train_selected", # A more descriptive name
-                name="filter_training_data_by_features",
+                func=feature_selection,
+                inputs=["X_train","y_train","params:feature_selection"],
+                outputs="X_cols",
+                name="model_feature_selection",
             ),
         ]
     )
